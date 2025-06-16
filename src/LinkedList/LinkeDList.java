79151e8880c9ef462ba879807b81abb0974ab23f
Sameer <ssgnhss@gmail.com>
2025-06-16 17:48:03 +0530

    Singly Linked List

---
+++ src/LinkedList/LinkeDList.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+public class LinkeDList {
+    public static void main(String[] args) {
+        LL ll=new LL();
+        ll.insertFirst(1);
+        ll.insertFirst(2);
+        ll.insertFirst(3);
+        ll.insertFirst(4);
+        ll.insertFirst(5);
+        ll.insertFirst(6);
+        ll.insert(32, 3);
+        ll.display();
+    }
+}
